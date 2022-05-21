e24c71b5e9bcd1e62db31c005f10972fcefcc0a4
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-05-21 17:09:29 +0700

    setup slice to handle login

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
+import { Button } from '@material-ui/core';
 import cityApi from 'api/cityApi';
+import { useAppDispatch } from 'app/hooks';
 import { NotFound, PrivateRoute } from 'components/Common';
 import { AdminLayout } from 'components/Layout';
+import { authActions } from 'features/auth/authSlice';
 import LoginPage from 'features/auth/pages/LoginPage';
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
+  const dispatch = useAppDispatch();
+
   useEffect(() => {
     cityApi.getAll().then((data) => console.log(data));
   });
 
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route element={<PrivateRoute />}>
-        <Route path="/admin" element={<AdminLayout />} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <>
+      <Button variant="contained" color="primary" onClick={() => dispatch(authActions.logout())}>
+        Log out
+      </Button>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/admin" element={<AdminLayout />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,5 +1,7 @@
 import { Box, Button, makeStyles, Paper, Typography } from '@material-ui/core';
+import { useAppDispatch } from 'app/hooks';
 import React from 'react';
+import { authActions } from '../authSlice';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -10,12 +12,23 @@ const useStyles = makeStyles((theme) => ({
     minHeight: '100vh',
   },
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(3),
   },
 }));
 
 export default function LoginPage() {
   const classes = useStyles();
+  const dispatch = useAppDispatch();
+
+  const handleLoginClick = () => {
+    // TODO: get username and password from login form
+    dispatch(
+      authActions.login({
+        username: 'test',
+        password: 'test',
+      })
+    );
+  };
 
   return (
     <div className={classes.root}>
@@ -25,7 +38,7 @@ export default function LoginPage() {
         </Typography>
 
         <Box style={{ marginTop: 32 }}>
-          <Button fullWidth variant="contained" color="primary">
+          <Button fullWidth variant="contained" color="primary" onClick={handleLoginClick}>
             Fake Login
           </Button>
         </Box>
